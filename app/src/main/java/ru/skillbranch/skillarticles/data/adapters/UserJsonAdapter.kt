3bd614806a7d94302538b2db7032743955ed0ce4
Yulka <tel961666@gmail.com>
2021-06-26 20:11:17 +0500

    User json adapter

--- app/src/main/java/ru/skillbranch/skillarticles/data/adapters/UserJsonAdapter.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/adapters/UserJsonAdapter.kt
@@ -4,13 +4,23 @@ import org.json.JSONObject
 import ru.skillbranch.skillarticles.data.local.User
 import ru.skillbranch.skillarticles.extensions.asMap
 
-class UserJsonAdapter() : JsonAdapter<User>{
+class UserJsonAdapter() : JsonAdapter<User> {
     override fun fromJson(json: String): User? {
-        TODO("Not yet implemented")
+        if (json.isEmpty()) return null
+        JSONObject(json).run {
+            val id = getString("id")
+            val name = getString("name")
+            val avatar: String? = if (!isNull("avatar")) getString("avatar") else null
+            val rating: Int = getInt("rating")
+            val respect: Int = getInt("respect")
+            val about: String? = if (!isNull("about")) getString("about") else null
+            return User(id, name, avatar, rating, respect, about)
+        }
     }
 
     override fun toJson(obj: User?): String {
-        TODO("Not yet implemented")
+        return obj?.let {
+            JSONObject(it.asMap()).toString()
+        } ?: ""
     }
-
 }
